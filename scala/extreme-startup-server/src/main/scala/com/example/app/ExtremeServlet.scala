e897d0f4b4fcb6327a6746036881c29de56bccfe
Robin Stephenson <robin.stephenson@mendeley.com>
2016-02-10 11:39:07 +0000

    parse query string

--- scala/extreme-startup-server/src/main/scala/com/example/app/ExtremeServlet.scala
+++ scala/extreme-startup-server/src/main/scala/com/example/app/ExtremeServlet.scala
@@ -1,17 +1,31 @@
 package com.example.app
-
-import org.scalatra._
-import scalate.ScalateSupport
+import java.lang.String
 
 class ExtremeServlet extends ExtremeStartupServerStack {
 
+  case class Question(id: String, question: String)
+
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    val q = params.get("q")
+    val query: Option[Question] = q match {
+      case None => None
+
+      case _ => {
+        val parts = q.get.split(": ")
+        if (parts.length != 2)
+          None
+        else {
+          val (id, question) = (parts(0), parts(1))
+          Option(Question(id, question))
+        }
+      }
+
+    }
+    query match {
+      case None => "無" // should be 400 bad request
+
+      case _ => "unanswerable question " + query.get.id + ": \"" + query.get.question + "\""
+    }
   }
 
 }
